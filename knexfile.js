04cf7e2f39477a137af5a0fa00134de0f3c19f55
Matt Works <mworks4905@gmail.com>
2016-11-10 18:17:04 -0700

    app file setup

--- knexfile.js
+++ knexfile.js
@@ -1,44 +1,16 @@
-// Update with your config settings.
+'use strict';
 
 module.exports = {
-
-  development: {
-    client: 'sqlite3',
-    connection: {
-      filename: './dev.sqlite3'
+    development: {
+        client: 'pg',
+        connection: 'postgres://localhost/reddit_clone'
+    },
+    test: {
+        client: 'pg',
+        connection: 'postgres://localhost/reddit_clone'
+    },
+    production: {
+        client: 'pg',
+        connection: process.env.DATABASE_URL
     }
-  },
-
-  staging: {
-    client: 'postgresql',
-    connection: {
-      database: 'my_db',
-      user:     'username',
-      password: 'password'
-    },
-    pool: {
-      min: 2,
-      max: 10
-    },
-    migrations: {
-      tableName: 'knex_migrations'
-    }
-  },
-
-  production: {
-    client: 'postgresql',
-    connection: {
-      database: 'my_db',
-      user:     'username',
-      password: 'password'
-    },
-    pool: {
-      min: 2,
-      max: 10
-    },
-    migrations: {
-      tableName: 'knex_migrations'
-    }
-  }
-
 };
